13c1d8dca26abe3242f320bc4681ad7872ab3958
tinogomes <tinorj@gmail.com>
2016-08-19 19:45:57 -0300

    Rubocop code style

--- lib/firebase_rails/firebase_base.rb
+++ lib/firebase_rails/firebase_base.rb
@@ -1,28 +1,23 @@
 require 'firebase'
 
 class FirebaseBase
-
-  def is_firebase_class() end
+  def firebase_class?
+    true
+  end
 
   def save
-    FirebaseBase.firebase_request(:update,
-      "#{self.firebase_model}/#{self.id}",
-      self.as_firebase_json
-    )
+    update as_firebase_json
   end
 
   def update(params)
-    FirebaseBase.firebase_request(:update,
-      "#{self.firebase_model}/#{self.id}",
-      params
-    )
+    FirebaseBase.firebase_request :update, "#{firebase_model}/#{id}", params
   end
 
   def as_firebase_json
-    removable_attrs = ["id", "firebase_model"]
-    cloned_instance_vars = self.dup
+    removable_attrs = %w(id firebase_model)
+    cloned_instance_vars = dup
     removable_attrs.each do |attr|
-      if self.instance_variable_names.include?("@#{attr}")
+      if instance_variable_names.include?("@#{attr}")
         cloned_instance_vars.remove_instance_variable("@#{attr}")
       end
     end
@@ -30,41 +25,45 @@ class FirebaseBase
   end
 
   class << self
-
     # creates new firebase object based on params passed
     # and returns the id (firebase key) of the new firebase object
     def create(params)
       # firebase returns the new id (key) as "name". Stupid I think
-      firebase_id = firebase_request(:push, firebase_model, params)["name"]
+      firebase_response = firebase_request(:push, firebase_model, params)
+      firebase_id = firebase_response['name']
       firebase_hash = normalize_firebase_hash(params, firebase_id)
+
       create_firebase_object(firebase_hash)
     end
 
     def destroy_all
-      firebase_response = firebase_request(:delete, "#{firebase_model}")
+      firebase_request(:delete, firebase_model.to_s)
     end
 
     # takes firebase id (string), and returns single object body
     def find(id)
       firebase_response = firebase_request(:get, "#{firebase_model}/#{id}")
       firebase_hash = normalize_firebase_hash(firebase_response, id)
+
       create_firebase_object(firebase_hash)
     end
 
     def all
-      firebase_response = firebase_request(:get, "#{firebase_model}")
+      firebase_response = firebase_request(:get, firebase_model.to_s)
       return [] unless firebase_response
+
       firebase_hashes_array = normalize_firebase_hashes(firebase_response)
-      firebase_hashes_array.map{ |hash| create_firebase_object(hash) }
+
+      firebase_hashes_array.map { |hash| create_firebase_object(hash) }
     end
 
     # firebase requires this type of encoding or it doesn't work
     # wack if you ask me, but it's in their docs somewhere
     def encode_init_value(value)
-      if value.kind_of?(String)
+      if value.is_a?(String)
         "\"#{value}\""
       elsif [true, false].include?(value)
-        "#{value}"
+        value.to_s
       end
     end
 
@@ -75,28 +74,30 @@ class FirebaseBase
       # so we have to grab all objects satisfying
       # first key/value
       init_key, init_value = hash.shift
-      firebase_response = firebase_request(:get, firebase_model,
-        :orderBy => "\"#{init_key}\"",
-        :equalTo => encode_init_value(init_value)
-      )
+      firebase_response = firebase_request :get,
+                                           firebase_model,
+                                           orderBy: "\"#{init_key}\"",
+                                           equalTo: encode_init_value(init_value)
 
       # we then filter the remaining key/values with ruby
-      hash.each do |k, v|
-        firebase_response = firebase_response.select do |firebase_key, firebase_hash|
-          firebase_hash[k.to_s] == v
+      hash.each do |key, value|
+        firebase_response = firebase_response.select do |_firebase_key, firebase_hash|
+          firebase_hash[key.to_s] == value
         end
       end
 
       firebase_hashes_array = normalize_firebase_hashes(firebase_response)
-      firebase_hashes_array.map{ |hash| create_firebase_object(hash) }
+
+      firebase_hashes_array.map { |hash_item| create_firebase_object(hash_item) }
     end
 
     def find_or_create_by(hash)
-      firebase_object = self.find_by(hash.dup)
+      firebase_object = find_by(hash.dup)
+
       if firebase_object.any?
         firebase_object.first
       else
-        self.create(hash)
+        create(hash)
       end
     end
 
@@ -117,21 +118,20 @@ class FirebaseBase
       associations_hash[:has_many] << attr.to_s
 
       define_method "set_#{attr}" do |args|
-        args = [args] unless args.kind_of?(Array)
+        args = [args] unless args.is_a?(Array)
         firebase_object_ids_hash = {}
         args.each do |object_or_id|
-          id = object_or_id.kind_of?(String) ? object_or_id : object_or_id.id
+          id = object_or_id.is_a?(String) ? object_or_id : object_or_id.id
           firebase_object_ids_hash[id] = true
         end
         self.class.set_attr_accessor(self, attr, firebase_object_ids_hash.keys)
-        FirebaseBase.firebase_request(:set,
-          "#{self.firebase_model}/#{self.id}/#{attr}",
-          firebase_object_ids_hash.as_json
-        )
+        FirebaseBase.firebase_request :set,
+                                      "#{firebase_model}/#{id}/#{attr}",
+                                      firebase_object_ids_hash.as_json
       end
 
       define_method "push_#{attr.to_s.singularize}" do |args|
-        self.send("set_#{attr}", self.transactions.push(args.id))
+        send("set_#{attr}", transactions.push(args.id))
       end
     end
 
@@ -139,7 +139,7 @@ class FirebaseBase
       associations_hash[:belongs_to] << attr.to_s
 
       define_method "set_#{attr}" do |args|
-        if args.class.method_defined? :is_firebase_class
+        if args.class.method_defined?(:firebase_class?)
           self.class.set_attr_accessor(self, attr, args.id)
         else
           self.class.set_attr_accessor(self, attr, args)
@@ -149,74 +149,66 @@ class FirebaseBase
 
     def set_attr_accessor(object, key, value)
       object.class_eval { attr_accessor key.to_s }
-      object.send("#{key.to_s}=", value)
+      object.send("#{key}=", value)
     end
 
-    def firebase_request(verb, path, params=nil)
+    def firebase_request(verb, path, params = nil)
       response = firebase_client.send(verb, path, params).body
 
       # firebase returns nil after successfully deleting records
-      if verb == :delete && !response
-        return true
-      end
+      return true if verb == :delete && !response
 
       # when trying to find all of a model and nothing exists
-      if verb == :get && !params && !response
-        return response
-      end
+      return response if verb == :get && !params && !response
 
       # if nil is returned firebase could not find the specified id
-      if verb != :delete && !response
-        throw "No object found at path: '#{path}'"
-      end
+      throw "No object found at path: '#{path}'" if verb != :delete && !response
 
       # specific error was thrown from firebase
-      if response["error"]
-        throw response["error"] if response["error"]
-      end
+      throw response['error'] if response['error']
 
       response
     end
 
     def firebase_client
-      @firebase_client ||= Firebase::Client.new("https://#{firebase_database_name}.firebaseio.com/")
+      @firebase_client ||= Firebase::Client.new(firebase_database_uri)
     end
 
-    def firebase_database_name
+    def firebase_database_uri
       name = ENV['FIREBASE_NAME']
-      throw "No firebase_name was found in the environment vars" if name.empty?
-      name
+      throw 'No FIREBASE_NAME was found in the environment vars' if name.empty?
+
+      "https://#{name}.firebaseio.com/"
     end
 
     def firebase_model
-      @firebase_model ||= self.to_s.underscore.gsub(/^firebase_/, '').pluralize
+      @firebase_model ||= to_s.underscore.gsub(/^firebase_/, '').pluralize
     end
 
     def normalize_firebase_hashes(objects_array)
-      objects_array.map{ |object| normalize_firebase_hash(object) }
+      objects_array.map { |object| normalize_firebase_hash(object) }
     end
 
     # we want to return every firebase object with the id (key)
     # included in a single hash.
     # Firebase returns object as arrays with the if as index 0
     # and data as index 1.
-    def normalize_firebase_hash(object, id=nil)
-      # firebase returns objects like this: ["-K6BRIuIfaHFb9aZDP7I", {"name"=>"Jordan", "age"=>"27"}]
-      if object.kind_of?(Array)
+    def normalize_firebase_hash(object, id = nil)
+      # firebase returns objects like this:
+      # ["-K6BRIuIfaHFb9aZDP7I", {"name"=>"Jordan", "age"=>"27"}]
+      if object.is_a?(Array)
         id = object[0]
         data = object[1]
-        data.merge({"id" => id})
-      elsif object.kind_of?(Hash) && id
-        object.merge({"id" => id})
+        data.merge('id' => id)
+      elsif object.is_a?(Hash) && id
+        object.merge('id' => id)
       end
     end
 
     def attr_accessors_hash(hash)
-      new_hash = {
-        firebase_model: firebase_model
-      }
+      new_hash = { firebase_model: firebase_model }
       # these are the keys that have values from the firebase object
-      included_keys = hash.keys.map{ |key| key.to_s }
+      included_keys = hash.keys.map(&:to_s)
 
       # goes through the associations and sets attr_accessors for those
       # associations that werent included from the firebase object
@@ -230,18 +222,19 @@ class FirebaseBase
       new_hash
     end
 
-    def formatted_value(k, v)
-      # firebase returns array of ids like this: {"-K8juhaua" => true, "-heddH9h" => true}
+    def formatted_value(key, value)
+      # firebase returns array of ids like this:
+      # {"-K8juhaua" => true, "-heddH9h" => true}
       # we need to format it as an array of ids
-      if associations_hash[:has_many].include?(k.to_s) && v.kind_of?(Hash)
-        v.keys
+      if associations_hash[:has_many].include?(key.to_s) && value.is_a?(Hash)
+        value.keys
       else
-        v
+        value
       end
     end
 
     def create_firebase_object(hash)
-      firebase_object = self.new
+      firebase_object = new
 
       # takes all attributes from firebase and possible attributes from
       # associations and creates and sets attr_accessors for them
@@ -253,4 +246,3 @@ class FirebaseBase
     end
   end
 end
-
--- test/firebase_base_test.rb
+++ test/firebase_base_test.rb
@@ -7,64 +7,64 @@ class FirebaseBaseTest < ActiveSupport::TestCase
   end
 
   def test_config_file
-    assert_equal "rails-tests", ENV['FIREBASE_NAME']
+    assert_equal 'rails-tests', ENV['FIREBASE_NAME']
   end
 
   def test_to_create_a_stock_object
-    stock = FirebaseStock.create({symbol: "APPL"})
-    assert_equal "APPL", stock.symbol
+    stock = FirebaseStock.create(symbol: 'APPL')
+    assert_equal 'APPL', stock.symbol
   end
 
   def test_to_find_single_stock_object_by_id
-    stock = FirebaseStock.create({symbol: "APPL"})
+    stock = FirebaseStock.create(symbol: 'APPL')
     found_stock = FirebaseStock.find(stock.id)
     assert_equal stock.id, found_stock.id
   end
 
   def test_getting_all_stocks
     3.times do |num|
-      FirebaseStock.create({symbol: num})
+      FirebaseStock.create(symbol: num)
     end
     stocks = FirebaseStock.all
     assert_equal 3, stocks.count
   end
 
   def test_find_by_param
-    stock_1 = FirebaseStock.create({symbol: "AAA"})
-    stock_2 = FirebaseStock.create({symbol: "BBB"})
-    found_stocks = FirebaseStock.find_by({symbol: "BBB"})
+    _stock_1 = FirebaseStock.create(symbol: 'AAA')
+    stock_2 = FirebaseStock.create(symbol: 'BBB')
+    found_stocks = FirebaseStock.find_by(symbol: 'BBB')
     assert_equal 1, found_stocks.count
     assert_equal stock_2.id, found_stocks.first.id
   end
 
   def test_find_by_boolean_param
-    stock_1 = FirebaseTransaction.create({open: false})
-    stock_2 = FirebaseTransaction.create({open: false})
-    stock_3 = FirebaseTransaction.create({open: true})
-    stock_4 = FirebaseTransaction.create({open: false})
-    found_stocks = FirebaseTransaction.find_by({open: true})
+    _stock_1 = FirebaseTransaction.create(open: false)
+    _stock_2 = FirebaseTransaction.create(open: false)
+    stock_3 = FirebaseTransaction.create(open: true)
+    _stock_4 = FirebaseTransaction.create(open: false)
+    found_stocks = FirebaseTransaction.find_by(open: true)
     assert_equal 1, found_stocks.count
     assert_equal stock_3.id, found_stocks.first.id
   end
 
   def test_find_by_multiple_params
-    stock_1 = FirebaseStock.create({symbol: "AAA", price: 3.44})
-    stock_2 = FirebaseStock.create({symbol: "BBB", price: 1.22})
-    stock_3 = FirebaseStock.create({symbol: "BBB", price: 1.33})
-    found_stocks = FirebaseStock.find_by({symbol: "BBB", price: 1.22})
+    _stock_1 = FirebaseStock.create(symbol: 'AAA', price: 3.44)
+    stock_2 = FirebaseStock.create(symbol: 'BBB', price: 1.22)
+    _stock_3 = FirebaseStock.create(symbol: 'BBB', price: 1.33)
+    found_stocks = FirebaseStock.find_by(symbol: 'BBB', price: 1.22)
     assert_equal 1, found_stocks.count
     assert_equal stock_2.id, found_stocks.first.id
   end
 
   def test_updating_firebase_object
-    stock = FirebaseStock.create({symbol: "AAA", price: 3.44})
-    stock.update({price: 5.01})
+    stock = FirebaseStock.create(symbol: 'AAA', price: 3.44)
+    stock.update(price: 5.01)
     found_stock = FirebaseStock.find(stock.id)
     assert_equal 5.01, found_stock.price
   end
 
   def test_saving_updates_for_firebase_object
-    stock = FirebaseStock.create({symbol: "AAA", price: 3.44})
+    stock = FirebaseStock.create(symbol: 'AAA', price: 3.44)
     stock.price = 6.01
     stock.save
     found_stock = FirebaseStock.find(stock.id)
@@ -72,76 +72,70 @@ class FirebaseBaseTest < ActiveSupport::TestCase
   end
 
   def test_retrieving_has_many_associations_when_they_dont_exist
-    stock = FirebaseStock.create({symbol: "AAA", price: 3.44})
+    stock = FirebaseStock.create(symbol: 'AAA', price: 3.44)
     assert_equal 0, stock.transactions.count
   end
 
   def test_has_many_associations
-    stock = FirebaseStock.create({symbol: "AAA", price: 3.44})
-    transactions = [1,2,3].map do |price|
-      FirebaseTransaction.create({price: price, stock: stock.id})
+    stock = FirebaseStock.create(symbol: 'AAA', price: 3.44)
+    transactions = [1, 2, 3].map do |price|
+      FirebaseTransaction.create(price: price, stock: stock.id)
     end
     stock.set_transactions(transactions)
     assert_equal 3, stock.transactions.count
   end
 
   def test_has_many_associations_return_array_of_ids
-    stock = FirebaseStock.create({symbol: "AAA", price: 3.44})
-    transactions = [1,2,3].map do |price|
-      FirebaseTransaction.create({price: price, stock: stock.id})
+    stock = FirebaseStock.create(symbol: 'AAA', price: 3.44)
+    transactions = [1, 2, 3].map do |price|
+      FirebaseTransaction.create(price: price, stock: stock.id)
     end
     stock.set_transactions(transactions)
     stock = FirebaseStock.find(stock.id)
-    assert stock.transactions.kind_of?(Array)
+    assert stock.transactions.is_a?(Array)
   end
 
   def test_has_many_associations_with_just_ids
-    stock = FirebaseStock.create({symbol: "AAA", price: 3.44})
-    transactions = [1,2,3].map do |price|
-      FirebaseTransaction.create({price: price, stock: stock.id})
+    stock = FirebaseStock.create(symbol: 'AAA', price: 3.44)
+    transactions = [1, 2, 3].map do |price|
+      FirebaseTransaction.create(price: price, stock: stock.id)
     end
-    stock.set_transactions(transactions.map{ |transaction| transaction.id })
+    stock.set_transactions(transactions.map(&:id))
     assert_equal 3, stock.transactions.count
   end
 
   def test_belongs_to_association_hard_coded
-    stock = FirebaseStock.create({symbol: "AAA"})
-    transactions = FirebaseTransaction.create({price: 1.22, stock: stock.id})
+    stock = FirebaseStock.create(symbol: 'AAA')
+    transactions = FirebaseTransaction.create(price: 1.22, stock: stock.id)
     assert_equal stock.id, transactions.stock
   end
 
   def test_belongs_to_association
-    stock = FirebaseStock.create({symbol: "AAA"})
-    transaction = FirebaseTransaction.create({price: 1.22})
+    stock = FirebaseStock.create(symbol: 'AAA')
+    transaction = FirebaseTransaction.create(price: 1.22)
     transaction.set_stock(stock)
     assert_equal stock.id, transaction.stock
   end
 
   def test_belongs_to_association_with_string
-    stock = FirebaseStock.create({symbol: "AAA"})
-    transaction = FirebaseTransaction.create({price: 1.22})
+    stock = FirebaseStock.create(symbol: 'AAA')
+    transaction = FirebaseTransaction.create(price: 1.22)
     transaction.set_stock(stock.id)
     assert_equal stock.id, transaction.stock
   end
 
-  def test_belongs_to_association_hard_coded
-    stock = FirebaseStock.create({symbol: "AAA"})
-    transaction = FirebaseTransaction.create({price: 1.22, stock: stock.id})
-    assert_equal stock.id, transaction.stock
-  end
-
   def test_pushing_an_object_into_a_has_many_association_with_existing_objects
-    transaction = FirebaseTransaction.create({price: 1.22})
-    stock = FirebaseStock.create({symbol: "AAA"})
+    transaction = FirebaseTransaction.create(price: 1.22)
+    stock = FirebaseStock.create(symbol: 'AAA')
     stock.set_transactions(transaction)
-    transaction_2 = FirebaseTransaction.create({price: 1.32, stock: stock.id})
+    transaction_2 = FirebaseTransaction.create(price: 1.32, stock: stock.id)
     stock.push_transaction(transaction_2)
     assert_equal stock.transactions.count, 2
     assert_equal stock.transactions.last, transaction_2.id
   end
 
   def test_find_or_create_by_when_object_exists
-    hash = {symbol: "AAA"}
+    hash = { symbol: 'AAA' }
     stock = FirebaseStock.create(hash)
     found_stock = FirebaseStock.find_or_create_by(hash)
     assert_equal stock.id, found_stock.id
@@ -149,7 +143,7 @@ class FirebaseBaseTest < ActiveSupport::TestCase
 
   def test_find_or_create_by_when_object_does_not_exists
     current_stock_count = FirebaseStock.all.count
-    FirebaseStock.find_or_create_by({symbol: "ABC"})
+    FirebaseStock.find_or_create_by(symbol: 'ABC')
     assert_equal current_stock_count + 1, FirebaseStock.all.count
   end
 end
